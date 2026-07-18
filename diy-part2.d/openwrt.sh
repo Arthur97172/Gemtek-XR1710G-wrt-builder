22d448f8dfd117aa2260d3182ed1dc5f0558692c
Arthur97172 <arthur97172@outlook.com>
2026-07-19 06:52:08 +0800

    Update openwrt.sh

--- diy-part2.d/openwrt.sh
+++ diy-part2.d/openwrt.sh
@@ -8,6 +8,7 @@ PKG_CONF="$GITHUB_WORKSPACE/packages/openwrt.conf"
 
 # ===== 修改内核选项示例 =====
 # sed -i '/CONFIG_PACKAGE_kmod-usb-ohci/d' .config
+sed -i '/CONFIG_PACKAGE_mihomo-alpha/d' .config
 
 # ===== UCI 默认值示例 ======
 # mkdir -p openwrt/files/etc/uci-defaults
